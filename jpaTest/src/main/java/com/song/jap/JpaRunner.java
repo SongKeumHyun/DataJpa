5c8448cb470ce644ec7b1391f0b32f77c00ff680
Song Keum Hyun <kumh1999@gmail.com>
2021-11-25 16:36:09 +0900

    init Account 성공

---
+++ jpaTest/src/main/java/com/song/jap/JpaRunner.java
@@ -0,0 +1,34 @@
+package com.song.jap;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		Account account = new Account();
+		account.setUsername("song2");
+		account.setPassword("1234");
+		
+		entityManager.persist(account);
+		
+
+
+		
+	}
+	
+
+}
